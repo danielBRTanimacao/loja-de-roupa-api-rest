c07cd6458052e02e797cde4b8c2a3ae7b7e4ff16
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-17 17:17:58 -0300

    new feature to main

--- hackerhank.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    dict_name_score: dict = {}
-    for _ in range(3):
-        name = input()
-        score = float(input())
-        dict_name_score.update({f'{name}': f'{score}'})
-        
-    print(dict_name_score)
