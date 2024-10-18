1f19e91734d3d680da4d1ab5706b8f73721678d2
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-18 08:12:56 -0300

    test with json

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return "<p>Hello, World!</p>"
+
+@app.route("/list")
+def hello_world():
+    with open('json/list.json', 'r') as file:
+        data = json.load(file)
+
+    return f"<p>{data}</p>"
+
+app.run()
