25a0c4505a293663e23d990a9921d68b85e0deb0
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-20 12:24:01 -0300

    update delete and update

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from api import views
 
 urlpatterns = [
     path('roupas/', views.ClothingView.as_view(), name='clothes'),
+    path('roupas/<int:id>/', views.ClothingViewUpdate.as_view(), name='update-clothes'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,7 +11,7 @@ class ClothingView(APIView):
     permission_classes = [AllowAny]
 
     def get_permissions(self):
-        if self.request.method == 'POST':
+        if self.request.method in ['POST', 'DELETE']:
             return [IsAuthenticated()]
         return super().get_permissions()
 
@@ -25,22 +26,15 @@ class ClothingView(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-    def delete(self, request):
-        ids = request.data.get("ids", [])
-        if not ids:
-            return Response(
-                {"detail": "Nenhum ID fornecido."},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        deleted, _ = Clothing.objects.filter(id__in=ids).delete()
-        if deleted:
-            return Response(
-                {"detail": f"{deleted} itens deletados com sucesso."},
-                status=status.HTTP_200_OK
-            )
-        else:
-            return Response(
-                {"detail": "Nenhum item encontrado para os IDs fornecidos."},
-                status=status.HTTP_404_NOT_FOUND
-            )
+    
+class ClothingViewUpdate(ClothingView):
+    def get(self, request, id):
+        clothes = get_object_or_404(Clothing, pk=id)
+        serializer = ClothingSerializer(clothes)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def delete(self, request, id):
+        clothing = get_object_or_404(Clothing, pk=id)
+        name = clothing.name
+        clothing.delete()
+        return Response({'message': f'Book deleted "{name}"'}, status=status.HTTP_204_NO_CONTENT)
